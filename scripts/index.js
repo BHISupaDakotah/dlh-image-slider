e18351f73f3517f14c047b042ffcba64985a4ce1
Dakotah Holmes <holmes.dakotah@gmail.com>
2022-05-25 14:26:10 -0600

    add image swipe fuctionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log("Hello from index.js");
+const slider = document.getElementById("slider");
+
+slider.addEventListener("input", (e) => {
+  const sliderValue = e.target.value;
+  const img = document.querySelector(".foreground-img");
+
+  img.style.width = `${sliderValue}%`;
+});
